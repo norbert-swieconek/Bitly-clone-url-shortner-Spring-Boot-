c96cca4757a22fed61c5d727f75718545ca2c10b
Norbert <norbert.swieconek@spartavity.com>
2026-02-27 14:49:09 +0100

    feat: add UrlMapping model

--- src/main/java/com/url_shortener_sb/models/UrlMapping.java
+++ src/main/java/com/url_shortener_sb/models/UrlMapping.java
@@ -1,4 +1,69 @@
 package com.url_shortener_sb.models;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
 public class UrlMapping {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String originalUrl;
+    private String shortUrl;
+    private int clickCount = 0;
+    private LocalDateTime createdDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "urlMapping")
+    private List<ClickEvent> clicEvents;
+
+    public UrlMapping() {}
+
+    public UrlMapping(String originalUrl, String shortUrl, LocalDateTime createdDate) {
+        this.originalUrl = originalUrl;
+        this.shortUrl = shortUrl;
+        this.createdDate = createdDate;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getOriginalUrl() {
+        return this.originalUrl;
+    }
+
+    public String getShortUrl() {
+        return this.shortUrl;
+    }
+
+    public int getClickCount() {
+        return this.clickCount;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return this.createdDate;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public void setShortUrl(String shortUrl) {
+        this.shortUrl = shortUrl;
+    }
+
+    public void setClickCount(int clickCount) {
+        this.clickCount = clickCount;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
 }
