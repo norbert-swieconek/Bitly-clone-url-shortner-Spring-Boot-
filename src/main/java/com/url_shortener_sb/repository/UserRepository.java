5cb3a7fb4be9f65a8cd8abadc0b303fafe6941f7
Norbert <norbert.swieconek@spartavity.com>
2026-02-28 15:55:29 +0100

    feat: implement jwt

---
+++ src/main/java/com/url_shortener_sb/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.url_shortener_sb.repository;
+
+import com.url_shortener_sb.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
