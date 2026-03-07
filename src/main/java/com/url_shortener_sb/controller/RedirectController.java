6a4a50ca88475e6fa0d72238273bec1f986b1d2c
Norbert <norbert.swieconek@spartavity.com>
2026-03-07 16:33:15 +0100

    feat: add redirect functionality and click count

---
+++ src/main/java/com/url_shortener_sb/controller/RedirectController.java
@@ -0,0 +1,30 @@
+package com.url_shortener_sb.controller;
+
+import com.url_shortener_sb.models.UrlMapping;
+import com.url_shortener_sb.service.UrlMappingService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RedirectController {
+    private UrlMappingService urlMappingService;
+
+    public RedirectController(UrlMappingService urlMappingService) {
+        this.urlMappingService = urlMappingService;
+    }
+
+    @GetMapping("/{shortUrl}")
+    public ResponseEntity<Void> redirect(@PathVariable String shortUrl) {
+        UrlMapping urlMapping = urlMappingService.getOriginalUrl(shortUrl);
+        if (urlMapping != null) {
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.add("Location", urlMapping.getOriginalUrl());
+            return ResponseEntity.status(302).headers(httpHeaders).build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
